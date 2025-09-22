3fe7cd68f7b2c612c412c48beea35f2fe187e9a2
smmazharul <hmoxygen@gmail.com>
2025-09-22 15:19:00 +0600

    check the other operators  with string variable

--- Basic-DSA/app.js
+++ Basic-DSA/app.js
@@ -10,3 +10,6 @@ console.log(a + parseInt(b));
 
 console.log("a and b are " + (a+b)) // this will concatenate a and b instead of adding them because of string in the beginning
 console.log(a+b+" a and b are ") // this will add a and b first then concatenate the result with string because string is at the end
+
+console.log("1"*1) // this will convert string to number and multiply them
+console.log("1"-1) // this will convert string to number and subtract them
