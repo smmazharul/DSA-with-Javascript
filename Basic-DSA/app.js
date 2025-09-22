a3e3214dee5b94b78dd103b623a09341b040544a
smmazharul <hmoxygen@gmail.com>
2025-09-22 13:36:33 +0600

    fix: change variable 'b' from string to number to prevent concatenation

--- Basic-DSA/app.js
+++ Basic-DSA/app.js
@@ -2,8 +2,10 @@
 // so we always use let and const instead of var
 
 let a = 5;
-let b = "10";
+let b = 10;
 console.log(a + parseInt(b));
 
 // here we tried to add number and string, so it will concatenate them instead of adding them
 // to fix this we can convert string to number using parseInt or Number function
+
+console.log("a and b are " + a+b) // this will concatenate a and b instead of adding them because of string in the beginning
