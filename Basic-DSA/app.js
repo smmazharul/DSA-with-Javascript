4ab9036180292336216febcb84c86d697525af81
smmazharul <hmoxygen@gmail.com>
2025-09-22 15:09:04 +0600

    fix: add parentheses to ensure correct addition of variables a and b

--- Basic-DSA/app.js
+++ Basic-DSA/app.js
@@ -8,4 +8,4 @@ console.log(a + parseInt(b));
 // here we tried to add number and string, so it will concatenate them instead of adding them
 // to fix this we can convert string to number using parseInt or Number function
 
-console.log("a and b are " + a+b) // this will concatenate a and b instead of adding them because of string in the beginning
+console.log("a and b are " + (a+b)) // this will concatenate a and b instead of adding them because of string in the beginning
