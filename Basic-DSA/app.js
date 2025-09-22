5438b39cfc590cb80dff874e79f6358033140416
smmazharul <hmoxygen@gmail.com>
2025-09-22 15:13:29 +0600

    fix: add console log to demonstrate addition and concatenation of variables a and b

--- Basic-DSA/app.js
+++ Basic-DSA/app.js
@@ -9,3 +9,4 @@ console.log(a + parseInt(b));
 // to fix this we can convert string to number using parseInt or Number function
 
 console.log("a and b are " + (a+b)) // this will concatenate a and b instead of adding them because of string in the beginning
+console.log(a+b+" a and b are ") // this will add a and b first then concatenate the result with string because string is at the end
